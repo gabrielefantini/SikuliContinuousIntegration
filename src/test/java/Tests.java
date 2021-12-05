82ee91cf3b0fe54b86beeedbd4268dd634b21eeb
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-05 20:38:26 +0100

    Try CI

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -10,7 +10,7 @@ public class Tests {
     @Test
     public void insertNewNote(){
         Screen s = new Screen();
-        String path = "C:\\"+"\\Users\\gabri\\InsertNewNote.sikuli\\";
+        String path = " file:///Users/runner/work/SikuliContinuousIntegration/SikuliContinuousIntegration/InsertNewNote.sikuli/";
         try{
             s.click(path + "1636537755998.png");
             s.click(path +"1636537782883.png");
