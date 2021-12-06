502739ab40147305d2c133d84167e19b0579308f
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-06 16:21:20 +0100

    Try CI

--- InstallAPKandRunTests.sh
+++ InstallAPKandRunTests.sh
@@ -1,8 +1,4 @@
 adb install app/OmniNotes-alphaDebug-6.1.0.apk
 sleep 1
 adb shell am start -n it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2> /dev/null
-brew install xquartz
-Xvfb :1337 &
-export DISPLAY=:1337 &
 ./gradlew test --info
