565e674ddd1e89adea00de094b2844996f6e0ee3
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-06 16:57:59 +0100

    Try CI

--- InstallAndRunXvfb.sh
+++ InstallAndRunXvfb.sh
@@ -1,4 +1,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2> /dev/null
 brew install xquartz
-Xvfb :1337 & export DISPLAY=:1337 &
 which xvbf
+Xvfb :1337 & export DISPLAY=:1337 &
