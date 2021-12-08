eb9630851952734da65098ab41e7ad584e34c9f1
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-08 13:21:28 +0100

    Try CI

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -13,7 +13,7 @@ public class Tests {
 
     @Test
     public void insertNewNote(){
-        ADBScreen s = new ADBScreen();
+        ADBScreen s = ADBScreen.start("/Users/runner/Library/Android/sdk/platform-tools/adb");
         Path relativePath = Paths.get("/Users/runner/Library/Android/sdk/platform-tools/adb");
         System.out.println(relativePath.toAbsolutePath().toString());
         String path = relativePath.toAbsolutePath().toString() + "/InsertNewNote.sikuli/";
