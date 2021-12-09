7ad4187a86b9805cb6eb97bdb04d18ed06bf5235
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-09 17:05:34 +0100

    Try CI

--- InstallAPKandRunTests.sh
+++ InstallAPKandRunTests.sh
@@ -2,6 +2,5 @@ adb install app/OmniNotes-alphaDebug-6.1.0.apk
 sleep 1
 adb shell am start -n it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity
 export DISPLAY=:1
-chmod +x screencapture
-screencapture /home/capture.jpg
+sudo screencapture screenshots/screenshot.jpg
 ./gradlew test --info
