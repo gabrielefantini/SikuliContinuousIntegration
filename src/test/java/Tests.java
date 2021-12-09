90070fd830ca5b3d43ccc6e9d3552a164b458b02
gabrielefantini <31799201+gabrielefantini@users.noreply.github.com>
2021-12-10 00:39:54 +0100

    Update Tests.java

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -16,6 +16,7 @@ public class Tests {
     @Test
     public void insertNewNote(){
         Screen s = new Screen();
+        Screen.showMonitots();
         Path relativePath = Paths.get("");
         System.out.println(relativePath.toAbsolutePath().toString());
         String path = relativePath.toAbsolutePath().toString() + "/InsertNewNote.sikuli/";
