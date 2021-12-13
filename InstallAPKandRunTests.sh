59b68e753cfd3ceb8d90b871e460eafe10f14a01
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-13 11:07:36 +0100

    Try CI

--- InstallAPKandRunTests.sh
+++ InstallAPKandRunTests.sh
@@ -1,7 +1,9 @@
+export DISPLAY=:1
+defaults write com.apple.dock single-app -bool true
+killall Dock
 adb install app/OmniNotes-alphaDebug-6.1.0.apk
 sleep 1
 adb shell am start -n it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity
-export DISPLAY=:1
 sleep 20
 screencapture screenshot1.png
 ./gradlew test --info
