8a2a06bc85d60a1325ec6d2dd6628d3e005a796e
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-16 17:47:42 +0100

    Try CI

--- InstallAPKandRunTests.sh
+++ InstallAPKandRunTests.sh
@@ -1,4 +1,5 @@
 export DISPLAY=:1
+cat ~/.android/avd/*.avd/config.ini
 adb install app/OmniNotes-alphaDebug-6.1.0.apk
 sleep 1
 adb shell am start -n it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity
