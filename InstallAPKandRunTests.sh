0f6417431a886168e535d89a140e70d5aae84c59
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-16 15:43:50 +0100

    Try CI

--- InstallAPKandRunTests.sh
+++ InstallAPKandRunTests.sh
@@ -1,6 +1,7 @@
 export DISPLAY=:1
 for f in ~/.android/avd/*.avd/config.ini; do echo 'hw.keyboard=yes' >> "$f"; done
 adb -e reboot
+sleep 180
 adb install app/OmniNotes-alphaDebug-6.1.0.apk
 sleep 1
 adb shell am start -n it.feio.android.omninotes.alpha/it.feio.android.omninotes.MainActivity
