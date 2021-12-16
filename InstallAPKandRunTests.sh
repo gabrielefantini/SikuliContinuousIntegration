b00e614fb90ad3b486b58846972f7d284c729254
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-16 15:59:51 +0100

    Try CI

--- InstallAPKandRunTests.sh
+++ InstallAPKandRunTests.sh
@@ -1,5 +1,6 @@
 export DISPLAY=:1
-for f in ~/.android/avd/*.avd/config.ini; do echo 'hw.keyboard=yes' >> "$f"; done
+for f in ~/.android/avd/*.avd/config.ini;do sed -i 's/hw.keyboard=no/hw.keyboard=yes/g' "$f"; done
+cat ~/.android/avd/*.avd/config.ini
 adb -e reboot
 sleep 180
 adb install app/OmniNotes-alphaDebug-6.1.0.apk
