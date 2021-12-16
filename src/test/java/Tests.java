761d02fe16ad0ad5c41879c11573dfd7689657cb
gabrielefantini <gabrielefantini97@gmail.com>
2021-12-16 18:22:42 +0100

    Try CI

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -15,33 +15,38 @@ public class Tests {
 
     public static int screenNumber = 0;
 
+
     @Test
-    public void insertNewNote(){
+    public void longTestRun(){
         Screen s = new Screen();
         Screen.showMonitors();
         Path relativePath = Paths.get("");
         System.out.println(relativePath.toAbsolutePath().toString());
-        String path = relativePath.toAbsolutePath().toString() + "/InsertNewNote.sikuli/";
+        String path = relativePath.toAbsolutePath().toString() + "/LongTestRun.sikuli/";
         screenshot();
         try{
             s.click(path + "emulator.png");
-            if(s.exists(path + "1636537755998.png", 1.0) == null){
+            if(s.exists(path + "1639648120667.png", 1.0) == null){
                 s.click(path + "emulator.png");
-                s.click(path + "1636537755998.png");
+                s.click(path + "1639648120667.png");
                 screenshot();
             }
             else {
-                s.click(path + "1636537755998.png");
+                s.click(path + "1639648120667.png");
                 screenshot();
             }
-            s.click(path + "1636537782883.png");
+            s.click(path + "1639648759992.png");
             screenshot();
-            s.click(path + "1636537803951.png");
+            s.click(path + "1639648168526.png");
             screenshot();
             Thread.sleep(1000);
-            s.type("New Note");
+            s.type("New CheckList");
             screenshot();
-            s.click(path + "1636537831061.png");
+            s.click(path + "1639648192318.png");
+            screenshot();
+            s.click(path + "1639648697457.png");
+            screenshot();
+            s.click(path + "1639648858695.png");
             screenshot();
             //Assert.assertTrue(s.exists(path+"1636537843807.png", 2.0) != null);
         }
