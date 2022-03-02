b4112dfc1ca417b99f7f6260be480e608406de06
gabrielefantini <gabrielefantini97@gmail.com>
2022-03-02 15:31:48 +0100

    6

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -214,7 +214,7 @@ public class Tests {
     */
 
     //not ok
-    /*@Test
+    @Test
     public void sortNotes() {
         Screen s = new Screen();
         Screen.showMonitors();
@@ -261,51 +261,52 @@ public class Tests {
             e.printStackTrace();
             screenshot();
         }
-    }*/
+    }
 
-      @Test
-        public void insertNewCategory() {
-            Screen s = new Screen();
-            Screen.showMonitors();
-            Path relativePath = Paths.get("");
-            System.out.println(relativePath.toAbsolutePath().toString());
-            String path = relativePath.toAbsolutePath().toString() + "/InsertNewCategory.sikuli/";
-            screenshot();
-            try{
+    //not ok
+    /*@Test
+    public void insertNewCategory() {
+        Screen s = new Screen();
+        Screen.showMonitors();
+        Path relativePath = Paths.get("");
+        System.out.println(relativePath.toAbsolutePath().toString());
+        String path = relativePath.toAbsolutePath().toString() + "/InsertNewCategory.sikuli/";
+        screenshot();
+        try{
+            s.click(path + "emulator.png");
+            if(s.exists(path + "1646138404642.png", 1.0) == null){
                 s.click(path + "emulator.png");
-                if(s.exists(path + "1646138404642.png", 1.0) == null){
-                    s.click(path + "emulator.png");
-                    s.click(path + "1646138404642.png");
-                }
-                else {
-                    s.click(path + "1646138404642.png");
-                }
-                s.click(path + "1646138418301.png");
-                s.click(path + "1646216730595.png");
-                s.click(path + "1646216745417.png");
-                s.click(path + "1646216760801.png");
-                screenshot();
-                s.click(path + "1646216777931.png");
-                s.click(path + "1646216795912.png");
-                s.click(path + "1646216810135.png");
-                s.click(path + "1646216819055.png");
-
-                if(s.exists(path + "1646155682237.png", 1.0) == null)
-                    s.click(path + "1646155682237.png");
-                s.type("New Note");
-
-                s.click(path + "1646138452543.png");
-
-                screenshot();
-                Assert.assertNotNull(s.exists(path + "1646216854386.png", 2.0));
-                s.click(path + "1646222369476.png");
-                Assert.assertNotNull(s.exists(path + "1646216863742.png", 2.0));
-                cleanAppState();
+                s.click(path + "1646138404642.png");
             }
-            catch(FindFailed | InterruptedException | IOException e){
-                e.printStackTrace();
+            else {
+                s.click(path + "1646138404642.png");
             }
+            s.click(path + "1646138418301.png");
+            s.click(path + "1646216730595.png");
+            s.click(path + "1646216745417.png");
+            s.click(path + "1646216760801.png");
+            screenshot();
+            s.click(path + "1646216777931.png");
+            s.click(path + "1646216795912.png");
+            s.click(path + "1646216810135.png");
+            s.click(path + "1646216819055.png");
+
+            if(s.exists(path + "1646155682237.png", 1.0) == null)
+                s.click(path + "1646155682237.png");
+            s.type("New Note");
+
+            s.click(path + "1646138452543.png");
+
+            screenshot();
+            Assert.assertNotNull(s.exists(path + "1646216854386.png", 2.0));
+            s.click(path + "1646222369476.png");
+            Assert.assertNotNull(s.exists(path + "1646216863742.png", 2.0));
+            cleanAppState();
         }
+        catch(FindFailed | InterruptedException | IOException e){
+            e.printStackTrace();
+        }
+    }*/
 
     /*@Test
     public void deleteCategory() {
